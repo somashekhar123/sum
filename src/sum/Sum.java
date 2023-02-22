50457faa2eabd5a87d3e2cf4b1a2680d5d71f1c6
SOMU SK <somashekharsk226@gmail.com>
2023-02-22 21:11:20 +0530

    somu

---
+++ src/sum/Sum.java
@@ -0,0 +1,15 @@
+package sum;
+
+public class Sum {
+
+	public static void main(String[] args) {
+	int[] i= {1,2,3};
+	int sum=0;
+	
+	for(int x=0;x<i.length;x++)
+	{
+sum=sum+i[x];
+	}
+System.out.println(sum);
+}
+}
